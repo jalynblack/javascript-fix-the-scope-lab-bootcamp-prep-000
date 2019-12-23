ed5873a059c4ec24e67858e2669209de9afbe683
jalynblack1 <jalynblack1@gmail.com>
2019-12-23 17:47:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 
-var theFunk = console.log(`funkyFunction`);
+var theFunk = console.log(funkyFunction());
 
 
